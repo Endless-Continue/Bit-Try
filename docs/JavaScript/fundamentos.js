afd9513d653c4a111cfba09dd8a4eb21b6786b51
Endless-Continue <javiertorresarrieta01@gmail.com>
2024-03-07 19:51:52 -0500

    javascript

---
+++ docs/JavaScript/fundamentos.js
@@ -0,0 +1,46 @@
+/*
+este 
+es un
+mensaje
+con 
+varias 
+lineas
+*/
+/*Output shit
+alert("buenas noches")
+console.log("mensaje a la consolita buenos dias")
+document.write("mensaje en interfaz de usuario")
+*/
+/* variables */
+//declaraciones
+/* saludo1 = "Mono"
+var saludo2 = "mono2"
+var saludo30 = "monosexo"
+let saludo3 = "mono3"
+const saludo4 = "mono4"
+console.log(saludo1)
+console.log(saludo2)
+console.log(saludo30)
+console.log(saludo3)
+console.log(saludo4) */ 
+/* ambito de las variables(diferencias de let y var(nunca usar var)) */
+/*
+let variableGlobal = "variable global"
+console.log(variableGlobal)
+{
+let variableLocal = "variable local"
+console.log(variableLocal)
+}
+*/
+//diferencias let y const (const es un valor constante que no puede ser cambiado o variado por otra variable)
+/* let numero1 = 1
+const numero2 = 2
+numero1 = "1"
+//numero2 = "2"
+
+console.log(numero1)
+console.log(numero2) */
+
+/*modo estricto(esto es para usar siempre una variable como let o const*/
+//"use strict"
+
