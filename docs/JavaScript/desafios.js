0d76f215a72cca6302cb845b293b5803e54c0083
Endless-Continue <javiertorresarrieta01@gmail.com>
2024-03-12 22:02:23 -0500

    JavaScript

--- docs/JavaScript/desafios.js
+++ docs/JavaScript/desafios.js
@@ -1,34 +1,33 @@
 "use strict";
+
 //hacer que salude
-//console.log("hola")
-//document.write("hola2")
-/* Escribir su nombre
-const nombreUsuario = prompt("Escriba su nombre")
+/* console.log("hola");
+document.write("hola2"); */
+//- Escribir su nombre
+/* const nombreUsuario = prompt("Escriba su nombre");
+console.log(nombreUsuario);
+document.write(nombreUsuario); */
 
-console.log(nombreUsuario) */
+//saludar con nombre usando switch y if else
 
-/*saludar con nombre usando switch y if else
+//const entrada = prompt("ingrese su nombre");
 
-const entrada = prompt("ingrese su nombre");
+/*switch (entrada) {
+  case "":
+    document.write("Welcome!");
+    break;
+  case entrada:
+    true;
+    document.write("Hello " + entrada + " Welcome");
+    break;
+} */
 
-
-
-  switch(entrada){
-    case "":
-        document.write("Welcome!");
-    break
-    case entrada:true
-         document.write("Hello " + entrada + " Welcome");
-         break}
- */
-
-/* if(entrada){
-    document.write("hola! " + entrada + " welcome")
+/* if (entrada) {
+  document.write("hola! " + entrada + " welcome");
+} else {
+  document.write("hola bb");
 }
-else {
-    document.write("hola bb")
-}*/
-
+ */
 /* prubitas exoticas adjasdasjdasjd */
 
 /* const entrada = prompt("Ingresa tu nombre:");
@@ -64,7 +63,7 @@ let resultado = userName1 + " " + userName2
 
 document.write("Hola! " + resultado + " bienvenido") */
 
-//DESAFIO COMPLETADO YO SOY LA MONDA
+//DESAFIO COMPLETADO
 
 /* Interpolacion de variables: aca usas ``(altgr + cerrar }) para concatenar mas rapido
 const nombre6 = prompt("ingresa nombre");
@@ -73,32 +72,36 @@ document.write(`${nombre6} ${nombre7}`)*/
 
 // Hacer  un programa que reciba del usuario un numero y muestre si es cero, de otra manera que muestre si es positivo o si es negativo
 
-/* let num1 = Number(prompt("Ingrese un numero"))
+/* let num1 = Number(prompt("Ingrese un numero"));
 
-if (num1 == 0) {
+if (num1 === 0) {
   document.write(`el numero es cero`);
 } else if (num1 > 0) {
   document.write(`el numero ${num1} es positivo`);
- } else {
+} else if (num1 < 0) {
   document.write(`el numero ${num1} es negativo`);
- } 
+} else {
+  document.write(`No es un numero`);
+}
  */
-
 //Hacer un programa que reciba  del usuario 2 numeros y muestre cual es el numero mayor y cual es el numero menor
-
+/* 
 let num2 = parseFloat(prompt("Ingrese un numero"));
 let num3 = parseFloat(prompt("Ingrese otro numero"));
 
-if (num2 < num3) {
+if (isNaN(num2)) {
+  document.write("El valor ingresado no es un número");
+} else if (isNaN(num3)) {
+  document.write("El valor ingresado no es un número");
+} else if (num2 < num3) {
   document.write(`el numero ${num3} es mayor y el numero ${num2} es el menor`);
 } else if (num2 > num3) {
   document.write(`el numero ${num2} es mayor y el numero ${num3} es el menor`);
 } else {
   document.write("Los numeros son iguales");
-}
-
-// if NaN
-/* 
-if (isNaN(numero)) {
-  document.write('El valor ingresado no es un número');
-  */
+} */
+/*  
+if NaN
+ if (isNaN(numero)) {
+  document.write("El valor ingresado no es un número");
+ */
