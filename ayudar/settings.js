0d40f70848ce0fbcc2eda1ce5aca6b082aa23b82
Endless-Continue <javiertorresarrieta01@gmail.com>
2024-03-18 22:33:10 -0500

    css

---
+++ ayudar/settings.js
@@ -0,0 +1,14 @@
+"use strict";
+/*let entrada = prompt("ingresa tu nombre");
+console.log(entrada);
+switch (entrada) {
+  case "":
+    document.write("Hola vacio");
+    break;
+  case null:
+    document.write("hola cancelaste");
+    break;
+  default:
+    document.write("hola " + entrada + " como estas");
+    break;
+}*/
